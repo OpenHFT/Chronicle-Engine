588e618d8fc54bb1ad91c2931ba5009adf7a3a6c
Rob Austin <rob.austin@boundedbuffer>
2014-10-07 14:32:43 +0100

    initial version

---
+++ src/main/java/Example.java
@@ -0,0 +1,17 @@
+/**
+ * @author Rob Austin.
+ */
+public class Example {
+
+
+    /**
+     * adding some text here to test the java doc
+     */
+    public void doSomething() {
+        System.out.print("");
+    }
+
+    public static void main(String... args) {
+        System.out.print("An example");
+    }
+}
