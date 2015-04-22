074be7222b3dc90764269162adb83e0de5682d13
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-04-22 16:43:11 +0100

    CE-23 now supports map.get() on the server using the same map that wire uses

--- src/test/java/net/openhft/chronicle/engine/map/MapByNameTest.java
+++ src/test/java/net/openhft/chronicle/engine/map/MapByNameTest.java
@@ -3,6 +3,7 @@ package net.openhft.chronicle.engine.map;
 import junit.framework.Assert;
 import net.openhft.chronicle.engine.map.WireRemoteStatelessMapClientTest.RemoteMapSupplier;
 import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.MapWireConnectionHub;
 import net.openhft.chronicle.map.MapWireConnectionHubByName;
 import org.junit.Test;
 
@@ -16,7 +17,7 @@ import static org.junit.Assert.assertEquals;
 public class MapByNameTest {
 
     @Test
-    public void test() throws IOException {
+    public void testSizeUsingLocalMap() throws IOException {
 
         try (RemoteMapSupplier<Integer, CharSequence> r = new RemoteMapSupplier<>(Integer.class,
                 CharSequence.class)) {
@@ -35,6 +36,34 @@ public class MapByNameTest {
                     r.serverEndpoint().mapWireConnectionHub());
 
             Assert.assertEquals(1, test2.size());
+        }
+    }
+
+
+    @Test
+    public void testGetUsingLocalMap() throws IOException {
+
+        try (RemoteMapSupplier<Integer, CharSequence> r = new RemoteMapSupplier<>(Integer.class,
+                CharSequence.class)) {
+
+            // remote client map
+
+            ChronicleMap<Integer, CharSequence> clientMap = r.get();
+            clientMap.put(1, "hello");
+            assertEquals(1, clientMap.size());
+
+
+            // local server map
+            final MapWireConnectionHub mapWireConnectionHub = r.serverEndpoint().mapWireConnectionHub();
+            final MapWireConnectionHubByName<Integer, CharSequence> localServerMap = new
+                    MapWireConnectionHubByName<>(
+                    "test",
+                    CharSequence.class,
+                    Integer.class,
+                    mapWireConnectionHub);
+
+            Assert.assertEquals("hello", localServerMap.get(1));
+
         }
     }
 
