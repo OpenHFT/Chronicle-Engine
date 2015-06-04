311ab784b5599f8862c9f685f26c8e91d2b12e5f
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-06-04 13:10:31 +0100

    CE-31 Add an example how to get and set the SessionDetails.

--- src/test/java/net/openhft/chronicle/engine2/api/SessionProviderTest.java
+++ src/test/java/net/openhft/chronicle/engine2/api/SessionProviderTest.java
@@ -10,11 +10,19 @@ public class SessionProviderTest {
     @Test
     public void testAcquireSessionProvider() {
         SessionProvider sessionProvider = Chassis.getAsset("").getView(SessionProvider.class);
-        sessionProvider.set(new VanillaSessionDetails("userId"));
+        VanillaSessionDetails sessionDetails0 = new VanillaSessionDetails("userId");
+        MyInfo info0 = new MyInfo();
+        sessionDetails0.set(MyInfo.class, info0);
+        sessionProvider.set(sessionDetails0);
 
         // get me a node in the graph
         Asset asset = Chassis.acquireAsset("test", Void.class, null, null);
         SessionDetails sessionDetails = asset.root().getView(SessionProvider.class).get();
         assertEquals("userId", sessionDetails.userId());
+        assertEquals(info0, sessionDetails.get(MyInfo.class));
+    }
+
+    static class MyInfo {
+        // place application specific session details here.
     }
 }
