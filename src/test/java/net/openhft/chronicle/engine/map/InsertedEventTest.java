3625cbf6259e69f4916ccfcb8b976b7636bc1a66
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-06-12 20:02:56 +0100

    Improve serialization of MapEvents.

--- src/test/java/net/openhft/chronicle/engine/map/InsertedEventTest.java
+++ src/test/java/net/openhft/chronicle/engine/map/InsertedEventTest.java
@@ -1,8 +1,11 @@
 package net.openhft.chronicle.engine.map;
 
 import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.bytes.BytesStore;
 import net.openhft.chronicle.engine.Factor;
+import net.openhft.chronicle.wire.BinaryWire;
 import net.openhft.chronicle.wire.TextWire;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -37,4 +40,71 @@ public class InsertedEventTest {
         assertEquals(insertedEvent, ie);
     }
 
+
+    @Test
+    @Ignore("TODO Fix")
+    public void testMarshalling3a() {
+        Bytes bytes = Bytes.elasticByteBuffer();
+        InsertedEvent<String, BytesStore> insertedEvent = InsertedEvent.of("asset", "key", BytesStore.wrap("£Hello World".getBytes()));
+        TextWire textWire = new TextWire(bytes);
+        textWire.write(() -> "reply")
+                .typedMarshallable(insertedEvent);
+        bytes.flip();
+        System.out.println("text: " + bytes);
+        InsertedEvent ie = (InsertedEvent) textWire.read(() -> "reply").typedMarshallable();
+        assertEquals(insertedEvent, ie);
+    }
+
+    @Test
+    @Ignore("TODO Fix")
+    public void testMarshalling3() {
+        Bytes bytes = Bytes.elasticByteBuffer();
+        InsertedEvent<String, BytesStore> insertedEvent = InsertedEvent.of("asset", "key", BytesStore.wrap("Hello World".getBytes()));
+        TextWire textWire = new TextWire(bytes);
+        textWire.write(() -> "reply")
+                .typedMarshallable(insertedEvent);
+        bytes.flip();
+        System.out.println("text: " + bytes);
+        InsertedEvent ie = (InsertedEvent) textWire.read(() -> "reply").typedMarshallable();
+        assertEquals(insertedEvent, ie);
+    }
+
+    @Test
+    public void testMarshallingB() {
+        Bytes bytes = Bytes.elasticByteBuffer();
+        InsertedEvent<String, String> insertedEvent = InsertedEvent.of("asset", "key", "name");
+        BinaryWire binaryWire = new BinaryWire(bytes);
+        binaryWire.write(() -> "reply").typedMarshallable(insertedEvent);
+        bytes.flip();
+        System.out.println("text: " + bytes);
+        InsertedEvent ie = (InsertedEvent) binaryWire.read(() -> "reply").typedMarshallable();
+        assertEquals(insertedEvent, ie);
+    }
+
+    @Test
+    public void testMarshalling2B() {
+        Bytes bytes = Bytes.elasticByteBuffer();
+        InsertedEvent<String, Factor> insertedEvent = InsertedEvent.of("asset", "key", new Factor());
+        BinaryWire binaryWire = new BinaryWire(bytes);
+        binaryWire.write(() -> "reply")
+                .typedMarshallable(insertedEvent);
+        bytes.flip();
+        System.out.println("text: " + bytes);
+        InsertedEvent ie = (InsertedEvent) binaryWire.read(() -> "reply").typedMarshallable();
+        assertEquals(insertedEvent, ie);
+    }
+
+
+    @Test
+    public void testMarshalling3B() {
+        Bytes bytes = Bytes.elasticByteBuffer();
+        InsertedEvent<String, BytesStore> insertedEvent = InsertedEvent.of("asset", "key", BytesStore.wrap("Hello World".getBytes()));
+        BinaryWire binaryWire = new BinaryWire(bytes);
+        binaryWire.write(() -> "reply")
+                .typedMarshallable(insertedEvent);
+        bytes.flip();
+        System.out.println("text: " + bytes);
+        InsertedEvent ie = (InsertedEvent) binaryWire.read(() -> "reply").typedMarshallable();
+        assertEquals(insertedEvent, ie);
+    }
 }
