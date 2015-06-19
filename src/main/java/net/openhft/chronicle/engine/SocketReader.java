cc010e2b5bd535d067fb6a471d180620b049290a
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-06-19 11:26:23 +0100

    OHFT-58 Change license to LGPLv3 to protect redistribution.

--- src/main/java/net/openhft/chronicle/engine/SocketReader.java
+++ src/main/java/net/openhft/chronicle/engine/SocketReader.java
@@ -1,8 +1,20 @@
-package net.openhft.chronicle.engine;
+/*
+ *     Copyright (C) 2015  higherfrequencytrading.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import java.util.Queue;
-import java.util.concurrent.*;
-import java.util.function.Supplier;
+package net.openhft.chronicle.engine;
 
 /**
  * Created by Rob on 17/06/15.
