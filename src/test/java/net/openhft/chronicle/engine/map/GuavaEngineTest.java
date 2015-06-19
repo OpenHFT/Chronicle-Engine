a887e8e3c5972b918d657b949bff594915290705
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-06-19 13:43:16 +0100

    CE-57 added the tests back in.

--- src/test/java/net/openhft/chronicle/engine/map/GuavaEngineTest.java
+++ src/test/java/net/openhft/chronicle/engine/map/GuavaEngineTest.java
@@ -1,17 +1,19 @@
 /*
- *     Copyright (C) 2015  higherfrequencytrading.com
+ * Copyright 2014 Higher Frequency Trading
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License.
+ * http://www.higherfrequencytrading.com
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package net.openhft.chronicle.engine.map;
@@ -49,7 +51,7 @@ public class GuavaEngineTest   {
     @NotNull
     public static Test suite() {
         AssetTree assetTree = new VanillaAssetTree().forTesting();
-/*
+
         TestSuite remoteMapTests = MapTestSuiteBuilder.using(new RemoteTestGenerator(assetTree))
                 .named("Chronicle RemoteEngine Guava tests")
                 .withFeatures(GENERAL_PURPOSE)
@@ -57,7 +59,6 @@ public class GuavaEngineTest   {
                 .withFeatures(CollectionFeature.REMOVE_OPERATIONS)
                 .withFeatures(RESTRICTS_KEYS, RESTRICTS_VALUES)
                 .createTestSuite();
-*/
 
         TestSuite localMapTests = MapTestSuiteBuilder.using(new LocalTestGenerator())
                 .named("Chronicle LocalEngine Guava tests")
@@ -68,8 +69,8 @@ public class GuavaEngineTest   {
                 .createTestSuite();
 
         TestSuite tests = new TestSuite();
-   //     tests.addTest(remoteMapTests);
-        // tests.addTest(localMapTests);
+        tests.addTest(remoteMapTests);
+        tests.addTest(localMapTests);
         return tests;
     }
 
