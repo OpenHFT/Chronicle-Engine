9cb8ea4e3b6f69eb2955fe1c0cd18bdc311455ed
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-07-22 22:09:51 +0300

    micro-tune writeUTF for String writing.

--- src/test/java/net/openhft/chronicle/engine/map/MapViewTest.java
+++ src/test/java/net/openhft/chronicle/engine/map/MapViewTest.java
@@ -17,13 +17,21 @@
 package net.openhft.chronicle.engine.map;
 
 import net.openhft.chronicle.engine.Chassis;
+import net.openhft.chronicle.engine.api.map.MapView;
 import net.openhft.chronicle.engine.api.set.KeySetView;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -49,5 +57,62 @@ public class MapViewTest {
         assertEquals(new HashSet<>(keys), keys);
         int hc = keys.hashCode();
         assertEquals(new HashSet<>(keys).hashCode(), hc);
+    }
+
+
+    //    @Test
+    public void testRemoteAccess() throws IOException {
+        Chassis.resetChassis();
+
+        MapView<String, UserInfo, UserInfo> userMap = Chassis.acquireMap("users", String.class, UserInfo.class);
+        userMap.put("userid", new UserInfo("User's Name"));
+
+        userMap.registerSubscriber(System.out::println);
+
+        // obtain just the fullName.
+        String fullName = userMap.applyToKey("userid", ui -> ui.fullName);
+
+        // increment a counter.
+        userMap.asyncUpdateKey("userid", ui -> {
+            ui.usageCounter++;
+            return ui;
+        });
+
+        // increment a counter and return the userid
+        int count = userMap.syncUpdateKey("userid",
+                ui -> {
+                    ui.usageCounter++;
+                    return ui;
+                },
+                ui -> ui.usageCounter);
+
+        Map<Integer, List<UserInfo>> collect = userMap.entrySet().query()
+                .filter(e -> e.getKey().matches("u*d"))
+                .map(e -> e.getValue())
+                .collect(Collectors.groupingBy(u -> u.usageCounter));
+
+// print userid which have a usageCounter > 10 each time it is incremented.
+        userMap.entrySet().query()
+                .filter(e -> e.getValue().usageCounter > 10)
+                .map(e -> e.getKey())
+                .subscribe(System.out::println);
+
+
+        Function<UserInfo, String> fullNameFunc = (Function<UserInfo, String> & Serializable) ui -> ui.fullName;
+//String fullName = userInfo.applyToKey("userid", fullNameFunc);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(fullNameFunc);
+        oos.close();
+        System.out.println("fullNameFunc.size=" + baos.toByteArray().length);
+    }
+}
+
+class UserInfo {
+    final String fullName;
+    int usageCounter;
+
+    UserInfo(String fullName) {
+        this.fullName = fullName;
     }
 }
