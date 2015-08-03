d70f4e3a8f2185c60d1de44432b7cfe0e9271611
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-08-03 09:22:03 +0100

    Revert "corrected the order of unsubsribe"
    This reverts commit e6a368780678df3fd5946f7dd2f8a97b09eb5bd7.

--- src/main/java/net/openhft/chronicle/engine/pubsub/RemoteReference.java
+++ src/main/java/net/openhft/chronicle/engine/pubsub/RemoteReference.java
@@ -93,10 +93,9 @@ public class RemoteReference<E> extends AbstractStatelessClient<ReferenceHandler
         Long subscriberTid = subscribersToTid.remove(subscriber);
         if (subscriberTid != null) {
             sendEventAsync(unregisterSubscriber, valueOut -> valueOut.int64(subscriberTid), false);
-            hub.unsubscribe(subscriberTid);
         } else
             LOG.warn("No subscriber to unsubscribe");
-
+        hub.unsubscribe(subscriberTid);
     }
 
     @Override
