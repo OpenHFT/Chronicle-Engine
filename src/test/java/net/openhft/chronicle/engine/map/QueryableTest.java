e2aa5d3f2370997a90684f439b950f18648d0094
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-08-07 16:14:04 +0100

    added support for queryable

---
+++ src/test/java/net/openhft/chronicle/engine/map/QueryableTest.java
@@ -0,0 +1,149 @@
+package net.openhft.chronicle.engine.map;
+
+import net.openhft.chronicle.core.Jvm;
+import net.openhft.chronicle.engine.ThreadMonitoringTest;
+import net.openhft.chronicle.engine.api.map.MapView;
+import net.openhft.chronicle.engine.api.query.Query;
+import net.openhft.chronicle.engine.api.tree.AssetTree;
+import net.openhft.chronicle.engine.server.ServerEndpoint;
+import net.openhft.chronicle.engine.tree.VanillaAssetTree;
+import net.openhft.chronicle.network.TCPRegistry;
+import net.openhft.chronicle.wire.WireType;
+import org.jetbrains.annotations.NotNull;
+import org.junit.*;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.stream.Collectors.averagingInt;
+import static net.openhft.chronicle.engine.Utils.methodName;
+
+/**
+ * @author Rob Austin.
+ */
+
+
+/**
+ * test using the listener both remotely or locally via the engine
+ *
+ * @author Rob Austin.
+ */
+@RunWith(value = Parameterized.class)
+public class QueryableTest extends ThreadMonitoringTest {
+    private static final String NAME = "test";
+    public String connection = "QueryableTest.host.port";
+    private static MapView<String, String> map;
+    private final Boolean isRemote;
+    private final WireType wireType;
+    @NotNull
+    @Rule
+    public TestName name = new TestName();
+    private AssetTree assetTree = new VanillaAssetTree().forTesting();
+    private VanillaAssetTree serverAssetTree;
+    private ServerEndpoint serverEndpoint;
+
+    public QueryableTest(Object isRemote, WireType wireType) {
+        this.isRemote = (Boolean) isRemote;
+        this.wireType = wireType;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() throws IOException {
+        return Arrays.asList(
+                new Object[]{Boolean.FALSE, WireType.BINARY},
+                new Object[]{Boolean.TRUE, WireType.BINARY},
+                new Object[]{Boolean.FALSE, WireType.TEXT},
+                new Object[]{Boolean.TRUE, WireType.TEXT}
+        );
+    }
+
+
+    @Before
+    public void before() throws IOException {
+        serverAssetTree = new VanillaAssetTree().forTesting();
+
+        if (isRemote) {
+
+            methodName(name.getMethodName());
+            connection = "StreamTest." + name.getMethodName() + ".host.port";
+            TCPRegistry.createServerSocketChannelFor(connection);
+            serverEndpoint = new ServerEndpoint(connection, serverAssetTree, wireType);
+            assetTree = new VanillaAssetTree().forRemoteAccess(connection, wireType);
+        } else
+            assetTree = serverAssetTree;
+
+        map = assetTree.acquireMap(NAME, String.class, String.class);
+    }
+
+    @After
+    public void after() throws IOException {
+        assetTree.close();
+        Jvm.pause(1000);
+        if (serverEndpoint != null)
+            serverEndpoint.close();
+        serverAssetTree.close();
+        if (map instanceof Closeable)
+            ((Closeable) map).close();
+        TCPRegistry.reset();
+    }
+
+    @Test
+    public void testQueryForEach() throws Exception {
+
+        final MapView<String, String> map = assetTree.acquireMap("name", String.class, String
+                .class);
+
+        map.put("1", "1");
+        map.put("2", "2");
+
+        final Query<String> query = map.keySet().query();
+
+        final Set<String> result = new HashSet<>();
+        query.forEach(result::add);
+
+        Assert.assertEquals(new HashSet<>(Arrays.asList("1", "2")), result);
+    }
+
+
+    @Test
+    public void testQueryForEachWithPredicate() throws Exception {
+
+        final MapView<String, String> map = assetTree.acquireMap("name", String.class, String
+                .class);
+
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+
+        final Query<String> query = map.keySet().query();
+
+        final Set<String> result = new HashSet<>();
+        query.filter("1"::equals).forEach(result::add);
+        Assert.assertEquals(new HashSet<>(Arrays.asList("1")), result);
+    }
+
+
+    @Test
+    public void testQueryForCollect() throws Exception {
+
+        final MapView<Integer, Integer> map = assetTree.acquireMap("name", Integer.class, Integer
+                .class);
+
+        map.put(1, 1);
+        map.put(2, 2);
+        map.put(3, 3);
+
+        final Query<Integer> query = map.keySet().query();
+        Double x = query.filter((obj) -> obj >= 1 && obj <= 2).collect(averagingInt(v -> (int) v));
+        Assert.assertEquals((Double) 1.5, x);
+    }
+
+}
+
