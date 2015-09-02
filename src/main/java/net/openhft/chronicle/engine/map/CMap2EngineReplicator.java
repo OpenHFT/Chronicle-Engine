9205c8fd14e47bd7361ff9d5751031b28fff5991
danielshaya <daniel@familyshaya.com>
2015-09-02 17:14:30 +0100

    Needed to catch the exception for class to compile.

--- src/main/java/net/openhft/chronicle/engine/map/CMap2EngineReplicator.java
+++ src/main/java/net/openhft/chronicle/engine/map/CMap2EngineReplicator.java
@@ -176,7 +176,11 @@ public class CMap2EngineReplicator implements EngineReplication,
 
             @Override
             public void dirtyEntries(final long fromTimeStamp) {
-                instance.dirtyEntries(fromTimeStamp);
+                try {
+                    instance.dirtyEntries(fromTimeStamp);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
 
             @Override
