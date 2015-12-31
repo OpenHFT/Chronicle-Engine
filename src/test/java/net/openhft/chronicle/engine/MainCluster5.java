42ae0529cf10a2aade2bdde4a8233c68d40bfd17
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-12-31 17:46:14 +0000

    added logging

--- src/test/java/net/openhft/chronicle/engine/MainCluster5.java
+++ src/test/java/net/openhft/chronicle/engine/MainCluster5.java
@@ -73,30 +73,30 @@ public class MainCluster5 {
             case "one":
 
                 tree = create(1, writeType, CLUSTER);
-                serverEndpoint = new ServerEndpoint(OS.getHostName() + ":8081", tree, writeType);
+                serverEndpoint = new ServerEndpoint("localhost:8081", tree, writeType);
                 break;
 
             case "two":
                 tree = create(2, writeType, CLUSTER);
-                serverEndpoint = new ServerEndpoint(OS.getHostName() + ":8082", tree, writeType);
+                serverEndpoint = new ServerEndpoint("localhost:8082", tree, writeType);
                 break;
 
 
             case "three":
                 tree = create(3, writeType, CLUSTER);
-                serverEndpoint = new ServerEndpoint(OS.getHostName() + ":8083", tree, writeType);
+                serverEndpoint = new ServerEndpoint("localhost:8083", tree, writeType);
                 break;
 
 
             case "four":
                 tree = create(4, writeType, CLUSTER);
-                serverEndpoint = new ServerEndpoint(OS.getHostName() + ":8084", tree, writeType);
+                serverEndpoint = new ServerEndpoint("localhost:8084", tree, writeType);
                 break;
 
 
             case "five":
                 tree = create(5, writeType, CLUSTER);
-                serverEndpoint = new ServerEndpoint(OS.getHostName() + ":8085", tree, writeType);
+                serverEndpoint = new ServerEndpoint("localhost:8085", tree, writeType);
                 break;
 
             case "client":
@@ -207,5 +207,7 @@ public class MainCluster5 {
         }
 
     }
+
+
 }
 
