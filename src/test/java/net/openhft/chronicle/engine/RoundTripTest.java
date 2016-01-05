06a5a048d64f677094e8e5ddad8ea96bc6e22c55
rob <rob.austin@boundedbuffer.com>
2016-01-05 14:21:53 +0000

    improved throughput

--- src/test/java/net/openhft/chronicle/engine/RoundTripTest.java
+++ src/test/java/net/openhft/chronicle/engine/RoundTripTest.java
@@ -40,7 +40,7 @@ import java.util.function.Function;
 public class RoundTripTest {
     public static final WireType WIRE_TYPE = WireType.BINARY;
     public static final int ENTRIES = 50;
-    public static final int TIMES = 1_000;
+    public static final int TIMES = 100;
     public static final String basePath = OS.TARGET + '/' + System.getProperty("server", "one");
     public static final String CLUSTER = System.getProperty("cluster", "clusterFive");
     static final int VALUE_SIZE = 2 << 20;
