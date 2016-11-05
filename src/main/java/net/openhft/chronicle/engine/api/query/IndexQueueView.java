c7595dc38952bbd17d365f738c98b3f046cd8629
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-11-05 12:33:49 +0000

    updated java doc

--- src/main/java/net/openhft/chronicle/engine/api/query/IndexQueueView.java
+++ src/main/java/net/openhft/chronicle/engine/api/query/IndexQueueView.java
@@ -23,27 +23,26 @@ import net.openhft.chronicle.wire.Marshallable;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Provides a snapshot ( some times referred to as an image ) of all the latest values and then
- * subsequent updates based on a filter.   The snapshot can be considered to work much like a map
- * does, where the latest values are returned based on the keys. You can specify the index from
- * where you want the data, so you will initially get the snapshot and then all subsequent changes.
- * The snapshot is Guaranteed to provide all the data for the current roll cycle that is none not to
- * then subsequently change up to the current end of the queue. *  If the server has not been
- * restarted then be snapshot is built up using the changes since the start of the cycle in which it
- * was started.  If the server is restarted and you ask for an index that before the current roll
- * cycle, the response then becomes the same as a filtered response from a queue. If you on the
- * other hand as for an index that's mid current cycle you will get a snapshot that is made up of
- * all the fields that are know not to subsequently change until the current end of the queue. Then
- * followed by all the changes from that point ( like a queue does), this way  once you have read
- * the queue to the end, your data model will always be fully updated for all your keys, regardless
- * of where in the current roll cycle you decide to replay the data from. If you specify the index
- * of 0 then the data will be replayed from the end of the queue and the snapshot will contain all
- * the latest values for all the keys.
- *
- So In summary : The  IndexQueueView is ideal if all your entries change each roll cycle,
- IndexQueueView is not able to give you a snapshot where the entries have not
- changed since the last roll cycle, such as yesterday.
-
+ * Based on a filter, Provides a snapshot ( some times referred to as an image ) of all the *
+ * latest values and then subsequent updates.   The snapshot can be considered to work much like *
+ * a map does, where the latest values are returned based on the keys. You can specify the index *
+ * from   where you want the data, so you will initially get the snapshot and then all subsequent
+ * changes.  Even if the server is restarted, The snapshot is Guaranteed to provide at least all
+ * the data for the * current roll cycle , the snapshot will always contain only the fields that are
+ * known not to then subsequently change up to the current end of the   queue. If the server has not
+ * been restarted then be snapshot is built up using the changes   since the start of the cycle in
+ * which it   was started.  If the server is restarted and you ask for an index that before the
+ * current roll  cycle, the response then becomes the same as a filtered response from a queue. If
+ * you on the * other hand as for an index that's mid current cycle you will get a snapshot that is
+ * made up of * all the fields that are know not to subsequently change until the current end of the
+ * queue. Then * followed by all the changes from that point ( like a queue does), this way  once
+ * you have read * the queue to the end, your data model will always be fully updated for all your
+ * keys, regardless   of where in the current roll cycle you decide to replay the data from. If you
+ * specify the index   of 0 then the data will be replayed from the end of the queue and the
+ * snapshot will contain all   the latest values for all the keys.   So In summary : The
+ * IndexQueueView is ideal if all your entries change each roll cycle,   IndexQueueView is not able
+ * to give you a snapshot where the entries have not * changed since the last roll cycle, such as
+ * yesterday.
  *
  * @author Rob Austin.
  */
