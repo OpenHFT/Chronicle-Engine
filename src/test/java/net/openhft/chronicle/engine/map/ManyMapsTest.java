27ef4a528943a14a61d26e9783286f3192a1ec87
Rob Austin <rob.austin@boundedbuffer.co.uk>
2017-02-25 11:58:58 +0000

    changed to try with resources

--- src/test/java/net/openhft/chronicle/engine/map/ManyMapsTest.java
+++ src/test/java/net/openhft/chronicle/engine/map/ManyMapsTest.java
@@ -39,9 +39,8 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
-/**
- * Created by daniel on 16/07/2015.
- */
+/
+
 public class ManyMapsTest {
 
     public static final String NAME =
@@ -71,6 +70,7 @@ public class ManyMapsTest {
     public void recordExceptions() {
         exceptions = Jvm.recordExceptions();
     }
+
     @After
     public void afterMethod() {
         if (!exceptions.isEmpty()) {
@@ -84,54 +84,56 @@ public class ManyMapsTest {
     public void testConnectToMultipleMapsUsingTheSamePort() throws IOException {
         int noOfMaps = 100;
         int noOfKvps = 100;
-        @NotNull String mapBaseName = "ManyMapsTest-";
-        @NotNull AssetTree assetTree = new VanillaAssetTree().forTesting();
-
-        @NotNull Map<String, Map<String, String>> _clientMaps = new HashMap<>();
-        TCPRegistry.createServerSocketChannelFor(NAME);
-        //TODO CHENT-68 Only works with BINARY NOT TEXT.
-        @NotNull ServerEndpoint serverEndpoint = new ServerEndpoint(NAME, assetTree);
-
-        @NotNull AssetTree clientAssetTree = new VanillaAssetTree().forRemoteAccess(NAME, WireType.BINARY);
-        System.out.println("Creating maps.");
-        @NotNull AtomicInteger count = new AtomicInteger();
-        IntStream.rangeClosed(1, noOfMaps).forEach(i -> {
-            @NotNull String mapName = mapBaseName + i;
-
-            @NotNull Map<String, String> map = clientAssetTree.acquireMap(mapName, String.class, String.class);
-
-            for (int j = 1; j <= noOfKvps; j++) {
-                map.put(getKey(mapName, j), getValue(mapName, j));
-            }
-
-            _clientMaps.put(mapName, map);
-            if (count.incrementAndGet() % 100 == 0)
-                System.out.print("... " + count);
-        });
-        System.out.println("...client maps " + noOfMaps + " Done.");
-
-        //Test that the number of maps created exist
-        Assert.assertEquals(noOfMaps, _clientMaps.size());
-
-        for (@NotNull Map.Entry<String, Map<String, String>> mapEntry : _clientMaps.entrySet()) {
-            System.out.println(mapEntry.getKey());
-            Map<String, String> map = mapEntry.getValue();
-
-            //Test that the number of key-value-pairs in the map matches the expected.
-            Assert.assertEquals(noOfKvps, map.size());
-
-            //Test that all the keys in this map contains the map name (ie. no other map's keys overlap).
-            String key = mapEntry.getKey();
-            @NotNull SerializablePredicate<String> stringPredicate = k -> !k.contains(key);
-            Assert.assertFalse(map.keySet().stream().anyMatch(stringPredicate));
-
-            //Test that all the values in this map contains the map name (ie. no other map's values overlap).
-            @NotNull SerializablePredicate<String> stringPredicate1 = v -> !v.contains(key);
-            Assert.assertFalse(map.values().stream().anyMatch(stringPredicate1));
-        }
-        clientAssetTree.close();
-        assetTree.close();
-        TcpChannelHub.closeAllHubs();
         TCPRegistry.reset();
+
+        @NotNull String mapBaseName = "ManyMapsTest-";
+        try (@NotNull AssetTree assetTree = new VanillaAssetTree().forTesting()) {
+
+            @NotNull Map<String, Map<String, String>> _clientMaps = new HashMap<>();
+            TCPRegistry.createServerSocketChannelFor(NAME);
+            //TODO CHENT-68 Only works with BINARY NOT TEXT.
+            @NotNull ServerEndpoint serverEndpoint = new ServerEndpoint(NAME, assetTree);
+
+            try (@NotNull AssetTree clientAssetTree = new VanillaAssetTree().forRemoteAccess(NAME, WireType.BINARY)) {
+                System.out.println("Creating maps.");
+                @NotNull AtomicInteger count = new AtomicInteger();
+                IntStream.rangeClosed(1, noOfMaps).forEach(i -> {
+                    @NotNull String mapName = mapBaseName + i;
+
+                    @NotNull Map<String, String> map = clientAssetTree.acquireMap(mapName, String.class, String.class);
+
+                    for (int j = 1; j <= noOfKvps; j++) {
+                        map.put(getKey(mapName, j), getValue(mapName, j));
+                    }
+
+                    _clientMaps.put(mapName, map);
+                    if (count.incrementAndGet() % 100 == 0)
+                        System.out.print("... " + count);
+                });
+                System.out.println("...client maps " + noOfMaps + " Done.");
+
+                //Test that the number of maps created exist
+                Assert.assertEquals(noOfMaps, _clientMaps.size());
+
+                for (@NotNull Map.Entry<String, Map<String, String>> mapEntry : _clientMaps.entrySet()) {
+                    System.out.println(mapEntry.getKey());
+                    Map<String, String> map = mapEntry.getValue();
+
+                    //Test that the number of key-value-pairs in the map matches the expected.
+                    Assert.assertEquals(noOfKvps, map.size());
+
+                    //Test that all the keys in this map contains the map name (ie. no other map's keys overlap).
+                    String key = mapEntry.getKey();
+                    @NotNull SerializablePredicate<String> stringPredicate = k -> !k.contains(key);
+                    Assert.assertFalse(map.keySet().stream().anyMatch(stringPredicate));
+
+                    //Test that all the values in this map contains the map name (ie. no other map's values overlap).
+                    @NotNull SerializablePredicate<String> stringPredicate1 = v -> !v.contains(key);
+                    Assert.assertFalse(map.values().stream().anyMatch(stringPredicate1));
+                }
+            }
+        }
+        TcpChannelHub.closeAllHubs();
+
     }
 }
